37074887dac6e9fa5eb50c25bb01c525cb384660
limiteds233 <ss2333_1@mail.ru>
2019-12-04 20:44:48 +0300

    add test

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,25 @@
 require 'test_helper'
 
-class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get static_pages_home_url
-    assert_response :success
-  end
+class StaticPagesControllerTest < ActionController::TestCase
 
-  test "should get help" do
-    get static_pages_help_url
+    test "should get home" do
+    get :home
     assert_response :success
-  end
+
+    end
+
+    test "should get help" do
+    get :help
+    assert_response :success
+
+    end
+
+    test "should get about" do
+    get :about
+    assert_response :success
+    
+    end
+
 
 end
+

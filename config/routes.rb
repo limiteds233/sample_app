675af91458802c17b5c38a68e91d54f1e9f1d5ef
limiteds233 <ss2333_1@mail.ru>
2019-12-16 17:33:44 +0300

    add sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
  
+  get 'sessions/new'
  get 'static_pages/home'
  get 'static_pages/help'
  get 'static_pages/about'
 get 'static_pages/contact'
 get 'users/new'
+get 'login' => 'sessions#new'
+post 'login' => 'sessions#create'
+delete 'logout' => 'sessions#destroy'
 
    resources :users
   

8b35fa83fb5c64f433826d024dcc172d89645355
limiteds233 <ss2333_1@mail.ru>
2019-12-16 18:12:06 +0300

    add val login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ class SessionsController < ApplicationController
         if user && user.authenticate(params[:session][:password])
 
         else
+        flash[:danger] = 'Invalid email/password combination'
         render 'new'
         end
     end

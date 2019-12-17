0e1b4e3916517fa6ebde2e2c4c6ae622c14479ab
limiteds233 <ss2333_1@mail.ru>
2019-12-17 10:19:45 +0300

    ref test

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "login with invalid information" do
+    get 'sessions#new'
+    assert_template 'sessions/new'
+    post login_path, session: { email: "", password: "" }
+    assert_template 'sessions/new'
+    assert_not flash.empty?
+    get 'users/new'
+    end
 end

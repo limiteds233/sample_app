aff280d7c5cfe9c580b8795bedd571095787520c
limiteds233 <ss2333_1@mail.ru>
2019-12-17 17:22:29 +0300

    add remember user

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,10 +3,26 @@ module SessionsHelper
     def log_in(user)
         session[:user_id] = user.id
     end
-# Возвращает текущего вошедшего пользователя (если есть).
-    def current_user
-        @current_user ||= User.find_by(id: session[:user_id])
+
+    def remember(user)
+        user.remember
+        cookies.permanent.signed[:user_id] = user.id
+        cookies.permanent[:remember_token] = user.remember_token
     end
+# Возвращает текущего вошедшего пользователя (если есть).
+    # Возвращает пользователя, соответствующего токену в cookie.
+    def current_user
+        if (user_id = session[:user_id])
+        @current_user ||= User.find_by(id: user_id)
+        elsif (user_id = cookies.signed[:user_id])
+    user = User.find_by(id: user_id)
+
+    if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+        end
+    end
+end
     # Возвращает true, если пользователь зарегистрирован, иначе возвращает false.
     def logged_in?
         !current_user.nil?
@@ -16,4 +32,6 @@ module SessionsHelper
         session.delete(:user_id)
         @current_user = nil
     end
+
+    
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+    attr_accessor :remember_token
 
     before_save { email.downcase! }
     validates :name, presence: true, length: { maximum: 50 }
@@ -17,4 +18,19 @@ class User < ActiveRecord::Base
         BCrypt::Password.create(string, cost: cost)
 
     end
+
+    def User.new_token
+        SecureRandom.urlsafe_base64
+    end
+
+    def remember
+       self.remember_token = User.new_token
+        update_attribute(:remember_digest, User.digest(remember_token))
+    end
+
+    # Возвращает true, если указанный токен соответствует дайджесту.
+    def authenticated?(remember_token)
+        BCrypt::Password.new(remember_digest).is_password?(remember_token)
+    end
+
 end

62fb7636fcd8b696ace0165e3b97db296f17a18a
limiteds233 <ss2333_1@mail.ru>
2019-12-20 09:48:44 +0300

    add test

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,8 @@ User.create!(name: "Example User",
 email: "example@railstutorial.org",
 password: "foobar",
 password_confirmation: "foobar")
+admin: true)
+
 99.times do |n|
 name = Faker::Name.name
 email = "example-#{n+1}@railstutorial.org"
@@ -17,5 +19,4 @@ User.create!(name: name,
 email: email,
 password: password,
 password_confirmation: password)
-admin: true)
 end

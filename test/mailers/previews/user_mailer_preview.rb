b161c7b4dc50465b22612d47db7a275331dcbde2
limiteds233 <ss2333_1@mail.ru>
2019-12-20 12:48:47 +0300

    add mailer

---
+++ test/mailers/previews/user_mailer_preview.rb
@@ -0,0 +1,16 @@
+# Предварительный просмотр всех писем по адресу:
+# http://localhost:3000/rails/mailers/user_mailer
+class UserMailerPreview <ActionMailer::Preview
+# Предварительный просмотр этого письма:
+# http://localhost:3000/rails/mailers/user_mailer/account_activation
+def account_activation
+    user = User.first
+    user.activation_token = User.new_token
+    UserMailer.account_activation(user)
+end
+# Предварительный просмотр этого письма:
+# http://localhost:3000/rails/mailers/user_mailer/password_reset
+def password_reset
+UserMailer.password_reset
+end
+end
